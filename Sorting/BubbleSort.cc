d3e3020f0cff23ed006ed9aaf7202c539f447ca9
Bob <babribhan@gmail.com>
2020-04-22 22:51:50 +0530

    Update BubbleSort.cc

--- Sorting/BubbleSort.cc
+++ Sorting/BubbleSort.cc
@@ -1,34 +1,34 @@
-/*
-Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements 
-and then swapping their positions if they exist in the wrong order.
-Complexity: 
-The complexity of bubble sort is O(n2) in both worst and average cases, because the entire array needs to be iterated for every element.
-
-Optimizaton: Bubble Sort can be usefull of Already or partially Sorted array in which case it will take O(n) time.
+/* Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements 
+   and then swapping their positions if they exist in the wrong order.
+   
+   Complexity:  The complexity of bubble sort is O(n2) in both worst and average cases,
+   because the entire array needs to be iterated for every element.
+   
+   Optimizaton: Bubble Sort can be usefull of Already or partially Sorted array in which 
+   case it will take O(n) time.
 */
 
-void swap(int &a, int &b){
-  if (a^b){
+void swap (int &a, int &b)
+{
+  if (a^b)
+  {
     a= a^b;
     b= a^b;
     a= a^b;
   }
 }
-void bubbleSort(int arr[], int n ) {
-  for ( int i = 0; i < n;++i){
-    for ( int j = 0; j <n-i;++j){
+void bubbleSortOpt (int arr[], int n ) 
+{
+  bool swapped = false;
+  for ( int i = 0; i < n;++i)
+  {
+    for ( int j = 0; j <n-i;++j)
+    {
       if ( arr[j] > arr[j+1])
-        swap(arr[j],arr[j+1]);
-    }
-  }
-}
-void bubbleSortOpt(int arr[], int n ) {
-  bool swapped=false;
-  for ( int i = 0; i < n;++i){
-    for ( int j = 0; j <n-i;++j){
-      if ( arr[j] > arr[j+1])
-        swap(arr[j],arr[j+1]);
-      swapped = true;
+      {
+        swap (arr[j],arr[j+1]);
+        swapped = true;
+      }
     }
     if(!swapped) // if swapped is false then the array is Already Sorted.
       return ;
