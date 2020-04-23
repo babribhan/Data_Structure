908ae7f1bd7c966d151092e21210e827fde8f976
Bob <babribhan@gmail.com>
2020-04-24 00:05:26 +0530

    Update MergeSort.cc

--- Sorting/MergeSort.cc
+++ Sorting/MergeSort.cc
@@ -15,25 +15,20 @@ void merge (vector<int> &arr, int start , int mid, int end )
         for ( int j = 0; j < n2 ; j++ )
                 right[j] = arr[mid+1+j];
 
-        i = 0;
-        j = 0;
+        i = j = 0;
         k = start;
-        while ( i < n1 && j < n2 ) {
-                //if ( left[i] <= right[j] ){
-                if ( left[i] > right[j] ){
-                        arr[k] = left[i++];
-                }
-                else {
-                        arr[k] = right[j++];
-                }
-                k++;
+        while ( i < n1 && j < n2 ) 
+        {
+                //if ( left[i] <= right[j] )
+                if ( left[i] > right[j] )
+                        arr[k++] = left[i++];
+                else 
+                        arr[k++] = right[j++];
         }
-        while ( i < n1 ) {
+        while ( i < n1 ) 
                 arr[k++] = left[i++];
-        }
-        while ( j < n2 ) {
+        while ( j < n2 ) 
                 arr[k++] = right[j++];
-        }
 }
 
 void merge_sort (vector<int> &arr, int start , int end ) 
