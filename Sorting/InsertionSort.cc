4cf449dc0eafc8f6b408dc5b0e905bfa9801243f
Bob <babribhan@gmail.com>
2020-04-23 22:40:44 +0530

    Update InsertionSort.cc

--- Sorting/InsertionSort.cc
+++ Sorting/InsertionSort.cc
@@ -1,10 +1,13 @@
 // Insertion Sort Algorithm
 
-void insertionSort(int arr[], int n ) {
-  for ( int i = 0; i < n;++i){
+void insertion_sort (int arr[], int n )
+{
+  for ( int i = 0; i < n;++i)
+  {
     int temp = arr[i];
     int j = i;
-    while (j > 0 && temp < arr[j-1]){
+    while (j > 0 && temp < arr[j-1])
+    {
       arr[j] = arr[j-1];
       j--;
     }
