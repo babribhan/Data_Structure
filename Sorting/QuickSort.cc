d1686bd218cd3c2da98a02d190fac4ac0e3eb80c
Bob <babribhan@gmail.com>
2020-04-24 01:08:52 +0530

    Update QuickSort.cc

--- Sorting/QuickSort.cc
+++ Sorting/QuickSort.cc
@@ -1,4 +1,4 @@
-int getPivot(vector<int> &arr, int start , int end ){
+int get_pivot (vector<int> &arr, int start , int end ){
         int pivot = arr[end];
         int i = start-1;
         for ( int j = start ; j<end ; j++){
@@ -11,10 +11,10 @@ int getPivot(vector<int> &arr, int start , int end ){
         return i+1;
 }
 
-void quickSort(vector<int> &arr, int start , int end ) {
+void quick_sort (vector<int> &arr, int start , int end ) {
         if ( start < end ) {
-                int p = getPivot(arr,start,end);
-                quickSort(arr,start,p-1);
-                quickSort(arr,p+1,end);
+                int p = get_pivot (arr,start,end);
+                quick_sort (arr,start,p-1);
+                quick_sort (arr,p+1,end);
         }
 }
